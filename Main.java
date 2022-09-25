c7e40b2d07dd117c2a62022bad51c654999acac9
VEMBUSELVAN <70207570+vems04@users.noreply.github.com>
2022-09-26 05:16:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 package com.vems.linkedlist;
 import java.util.*;
-
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        INSERTION o = new INSERTION();
+        OPERATIONS o = new OPERATIONS();
         int n,i;
         n = sc.nextInt();
         int [] a = new int [n];
@@ -12,11 +11,16 @@ public class Main {
             a[i]=sc.nextInt();
             o.insertLast(a[i]);
         }
+        o.insertFirst(85);
         o.insertatindex(155,3);
+        o.insertLast(58);
+        o.Deleteatfirst();
+        o.Deleteatlast();
         o.Display();
-
-
-    }}
+        o.DelteatIndex(1);
+        o.Display();
+    }
+}
 
 
 
